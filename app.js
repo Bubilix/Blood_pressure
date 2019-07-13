eb550222f7b1cce55311071b77035d0cb0c3da46
Sven <44684484+Bubilix@users.noreply.github.com>
2019-07-13 21:22:23 +0200

    html and css changed and relocated
    index.html adopted for style.css use and both relocated in their own GUI folder for further use

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (_req, res) => {
-    res.send('Hello World');
+app.use(express.static(path.join(__dirname, '/GUI')));
+
+app.get('/', (req, res) => {
+    if (res) {
+        res.status(200).sendFile(path.join(__dirname, '/GUI', 'index.html'));
+    } else {
+        res.status(404).write('Page not found!');
+    }
 });
 
 const port = process.env.PORT || 3000;

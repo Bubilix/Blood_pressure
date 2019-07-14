13003ec9f43fdcb0a6a2244c2229c874ae302f9f
Sven <44684484+Bubilix@users.noreply.github.com>
2019-07-14 16:43:22 +0200

    files rearanged
    After detailed review considering online tutorials I reorganized whole data structure and prepared the code for further adjustments for dynamic page layout.

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const DOMstrings = require('./GUI/DOMstrings');
-const UICtrl = require('.//Controlls.js/UIController');
+const DOMstrings = require('./public/DOMstrings');
+const UICtrl = require('./Controlls.js/UIController');
 
-app.use(express.static(path.join(__dirname, '/GUI')));
-app.use(express.static(path.join(__dirname, '/GUI', 'index.pug')));
 
+app.set('views', './views');
 app.set('view engine', 'pug');
-app.set('views', 'GUI');
 
 app.get('/', (req, res) => {
     if (res) {
-        //res.status(200).sendFile(path.join(__dirname, '/GUI', 'index.pug'));
-        res.render('index', { moj_tlak: 'Moj tlak'});
+        res.render('index');
     } else {
         res.status(404).write('Page not found!');
     }

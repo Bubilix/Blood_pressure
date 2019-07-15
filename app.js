102a3470af15bc5295417ad308b509bd3dc7dd62
Sven <44684484+Bubilix@users.noreply.github.com>
2019-07-15 17:43:19 +0200

    Code reorganized
    App reorganized so it works quite ok.
    .pug and .html file separated for possible later use.
    Still .css full functionality needs to be investigated and adopted for further app usage.

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ const UICtrl = require('./Controlls.js/UIController');
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname,'views', 'assets','css')));
 
 app.get('/', (req, res) => {
     if (res) {
-        res.render('index');
+        res.render('index', { nav_class: 'nonactive-nav', new_measuring_visibility: 'hidden'});
+        //res.sendFile('index.html', {root: __dirname+'/original_html'});
+    } else {
+        res.status(404).write('Page not found!');
+    }
+});
+app.get('/input_new_measuring', (req, res) => {
+    if (res) {
+        res.render('index', { nav_class: "active_nav", new_measuring_visibility: 'visible'});
     } else {
         res.status(404).write('Page not found!');
     }

cfe480bb2bf309954d4f021bed36ab79618ca645
Sven <44684484+Bubilix@users.noreply.github.com>
2019-08-07 20:54:41 +0200

    not important any more

---
+++ Middleware/DOMstrings.js
@@ -0,0 +1,11 @@
+module.export = DOMstrings = {
+    nav: 'nav',
+    newMeasure: '#new-measure',
+    arrayMeasures: '#new-multiple-measures',
+    graphicalChart: '#graphics',
+    newData: '.new-measurings',
+    upperPressureValueInput: '#upper-pressure-input-field',
+    lowerPressureValueInput: '#lower-pressure-input-field',
+    submitUpperPressure: '#submit-upper-pressure',
+    submitLowerPressure: '#submit-lower-pressure'
+  };
--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const DOMstrings = require('./public/DOMstrings');
+const UICtrl = require('./Controlls.js/UIController');
 
-app.get('/', (_req, res) => {
-    res.send('Hello World');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname,'views', 'assets','css')));
+
+app.get('/', (req, res) => {
+    if (res) {
+        res.render('index', { nav_class: 'nonactive-nav', new_measuring_visibility: 'hidden'});
+    } else {
+        res.status(404).write('Page not found!');
+    }
+});
+app.get('/input_new_measuring', (req, res) => {
+    if (res) {
+        res.render('index', { nav_class: "active-nav", new_measuring_visibility: 'visible'});
+    } else {
+        res.status(404).write('Page not found!');
+    }
 });
 
 const port = process.env.PORT || 3000;

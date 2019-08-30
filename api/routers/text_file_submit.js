c6e2443ff647ae8b740011f1caa3a2e4c27eac1a
Gasparovic <sven.gasparovic@magna.com>
2019-08-30 13:21:23 +0200

    filereader implemented

--- api/routers/text_file_submit.js
+++ api/routers/text_file_submit.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const browserSupport = require('../modules/browserSupport');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,6 +17,19 @@ const upload = multer({ storage: storage })
 router.post('/', upload.single('fileupload'), (req, res, next) => {
     if (res) {
       console.log(req.file);
+      if (browserSupport) {
+        //capture files after event is triggered
+        console.log('Browser supports...');
+        const reader = new FileReader();
+        reader.onload = function() {
+          console.log(reader.result);
+          // res.render('./assets/pugs/text_file_submit.pug', {
+          //   nav_class_input: 'active-nav1',
+          //   nav_class_show: 'hidden',
+          //   input_text: reader.readAsText(output)
+          // })
+        }
+      };
     } else {
       res.status(404).send('Nothing uploaded.');
     }

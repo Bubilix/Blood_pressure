b06b5d45a84c9cf73c882800129959cd6e21019a
Gasparovic <sven.gasparovic@magna.com>
2019-09-24 16:11:59 +0200

    Input values query in Users database not functional; needs update

--- api/modules/date_converter.js
+++ api/modules/date_converter.js
@@ -1,9 +1,17 @@
 module.exports = function date_converter(date) {
-    let date_array = date.split(".");
-    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const day = date_array[0];
-    const month = months[date_array[1] - 1];
-    const year = date_array[2];
-    //export date string in form "MM DD, YYYY 08:00:00 GMT" 
-    return (month + " " + day + ", " + year + " 08:00:00 GMT");
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    if(date.indexOf(".") !== -1) {
+        const date_array = date.split(".");
+        const day = date_array[0];
+        const month = months[date_array[1] - 1];
+        const year = date_array[2];
+        //export date string in form "MM DD, YYYY 08:00:00 GMT" 
+        return (month + " " + day + ", " + year + " 08:00:00 GMT");
+    } else {
+        const date_array = date.split("-");
+        const day = date_array[2];
+        const month = months[date_array[1] - 1];
+        const year = date_array[0];
+        return (month + " " + day + ", " + year + " 08:00:00 GMT");
+    }    
 }
--- api/routers/average.js
+++ api/routers/average.js
@@ -9,36 +9,28 @@ const dateConverter = require('../modules/date_converter');
 
 router.get('/', mongoose_connection, (req, res, next) => {
     if (res) {
-
-        Users.find({'inputs.time': req.query.period_begin, username: req.user.username}, function(err, user) {
-            if (err) {
-                res.send('Greška se pojavila prilikom učitavanja sadržaja.');
-            } else {
-                console.log(user);
-                if (user.inputs.length > 0) {
-                    const sortedData = sortingData(user.inputs);
-                    const renderData = renderingData(sortedData);
-                    const renderDataLimit = outputDataLimit(renderData, req.query.number_to_show);
-                    res.render('./assets/pugs/average_values.pug', {
-                        nav_class_input: "hidden",
-                        nav_class_show: 'active-nav2',
-                        sortData: renderDataLimit,
-                        begin: req.query.period_begin,
-                        end: req.query.period_end,
-                        number_to_show: req.query.number_to_show
-                    });
-                } else {
-                    res.send('Još nije unešeno niti jedno mjerenje!!!\nMolimo unesite barem jedno mjerenje da bismo mogli pokazati prosječne vrijednosti.');
-                }
-            }
-        });
-        // Users.findOne({username: req.user.username}, 'inputs', { time: req.query.period_begin}, function(err, user) {
+        // Users.aggregate([
+        //     { $group: {_id: null, upperValue: upperValue, lowerValue: lowerValue, time: req.query.period_begin},
+        //     $project: { _id: 0, upperValue: 1, lowerValue: 1, time: 0} }
+        // ]).then(function(result) {
+        //     console.log(result);
+        // }).
+        // catch(function(err) {console.log(err)});
+        Users.aggregate([
+            { $project: { _id: 0, username: 0, password: 0, inputs: 1}},
+            { $match: {"username": req.user.username}}
+            ]).
+            exec(function(err, result) {
+                console.log(result);
+            });
+        
+        // console.log(req.query.period_begin);
+        // console.log(dateConverter(req.query.period_begin));
+        // Users.find({username: req.user.username, 'inputs.time': 'Sep 20, 2019 08:00:00 GMT'}, function(err, user) {
         //     if (err) {
         //         res.send('Greška se pojavila prilikom učitavanja sadržaja.');
         //     } else {
         //         console.log(user);
-        //         const query = user.get('inputs');
-        //         const inputs = user.getInputs();
         //         if (user.inputs.length > 0) {
         //             const sortedData = sortingData(user.inputs);
         //             const renderData = renderingData(sortedData);

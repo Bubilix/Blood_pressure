20b16649e1942d1a5af3a9380d1ea5f5dbff5b24
Sven <44684484+Bubilix@users.noreply.github.com>
2019-09-24 21:35:15 +0200

    Users. find update in average module

--- api/routers/average.js
+++ api/routers/average.js
@@ -16,38 +16,39 @@ router.get('/', mongoose_connection, (req, res, next) => {
         //     console.log(result);
         // }).
         // catch(function(err) {console.log(err)});
-        Users.aggregate([
-            { $project: { _id: 0, username: 0, password: 0, inputs: 1}},
-            { $match: {"username": req.user.username}}
-            ]).
-            exec(function(err, result) {
-                console.log(result);
-            });
+        // Users.aggregate([
+        //     { $project: { _id: 0, username: 0, password: 0, inputs: 1}},
+        //     { $match: {"username": req.user.username}}
+        //     ]).
+        //     exec(function(err, result) {
+        //         console.log(result);
+        //     });
         
         // console.log(req.query.period_begin);
-        // console.log(dateConverter(req.query.period_begin));
-        // Users.find({username: req.user.username, 'inputs.time': 'Sep 20, 2019 08:00:00 GMT'}, function(err, user) {
-        //     if (err) {
-        //         res.send('Greška se pojavila prilikom učitavanja sadržaja.');
-        //     } else {
-        //         console.log(user);
-        //         if (user.inputs.length > 0) {
-        //             const sortedData = sortingData(user.inputs);
-        //             const renderData = renderingData(sortedData);
-        //             const renderDataLimit = outputDataLimit(renderData, req.query.number_to_show);
-        //             res.render('./assets/pugs/average_values.pug', {
-        //                 nav_class_input: "hidden",
-        //                 nav_class_show: 'active-nav2',
-        //                 sortData: renderDataLimit,
-        //                 begin: req.query.period_begin,
-        //                 end: req.query.period_end,
-        //                 number_to_show: req.query.number_to_show
-        //             });
-        //         } else {
-        //             res.send('Još nije unešeno niti jedno mjerenje!!!\nMolimo unesite barem jedno mjerenje da bismo mogli pokazati prosječne vrijednosti.');
-        //         }
-        //     }
-        // });
+        // console.log(dateConverter(req.query.period_begin));, 'inputs.time': 'Sep 20, 2019 08:00:00 GMT'
+        Users.find({username: req.user.username, 'inputs.time': 'Sep 20, 2019 08:00:00 GMT'}, function(err, user) {
+            if (err) {
+                res.send('Greška se pojavila prilikom učitavanja sadržaja.');
+            } else {
+                console.log(user[0]);
+                console.log(user[0].inputs);
+                if (user[0].inputs.length > 0) {
+                    const sortedData = sortingData(user[0].inputs);
+                    const renderData = renderingData(sortedData);
+                    const renderDataLimit = outputDataLimit(renderData, req.query.number_to_show);
+                    res.render('./assets/pugs/average_values.pug', {
+                        nav_class_input: "hidden",
+                        nav_class_show: 'active-nav2',
+                        sortData: renderDataLimit,
+                        begin: req.query.period_begin,
+                        end: req.query.period_end,
+                        number_to_show: req.query.number_to_show
+                    });
+                } else {
+                    res.send('Još nije unešeno niti jedno mjerenje!!!\nMolimo unesite barem jedno mjerenje da bismo mogli pokazati prosječne vrijednosti.');
+                }
+            }
+        });
     } else {
         res.status(404).send('Access to server denied!');
     }

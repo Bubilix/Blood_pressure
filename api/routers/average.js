ed77fcdfe6bb2e5a162de9a5dbf4794f496c77a0
Sven <44684484+Bubilix@users.noreply.github.com>
2019-09-25 22:32:43 +0200

    Small changes

--- api/routers/average.js
+++ api/routers/average.js
@@ -26,27 +26,27 @@ router.get('/', mongoose_connection, (req, res, next) => {
         
         // console.log(req.query.period_begin);
         // console.log(dateConverter(req.query.period_begin));, 'inputs.time': 'Sep 20, 2019 08:00:00 GMT'
-        Users.find({username: req.user.username, 'inputs.time': 'Sep 20, 2019 08:00:00 GMT'}, function(err, user) {
+        Users.find({username: req.user.username, 'inputs.upperValue': '132'}, function(err, user) {
             if (err) {
                 res.send('Greška se pojavila prilikom učitavanja sadržaja.');
             } else {
                 console.log(user[0]);
                 console.log(user[0].inputs);
-                if (user[0].inputs.length > 0) {
-                    const sortedData = sortingData(user[0].inputs);
-                    const renderData = renderingData(sortedData);
-                    const renderDataLimit = outputDataLimit(renderData, req.query.number_to_show);
-                    res.render('./assets/pugs/average_values.pug', {
-                        nav_class_input: "hidden",
-                        nav_class_show: 'active-nav2',
-                        sortData: renderDataLimit,
-                        begin: req.query.period_begin,
-                        end: req.query.period_end,
-                        number_to_show: req.query.number_to_show
-                    });
-                } else {
-                    res.send('Još nije unešeno niti jedno mjerenje!!!\nMolimo unesite barem jedno mjerenje da bismo mogli pokazati prosječne vrijednosti.');
-                }
+                // if (user[0].inputs.length > 0) {
+                //     const sortedData = sortingData(user[0].inputs);
+                //     const renderData = renderingData(sortedData);
+                //     const renderDataLimit = outputDataLimit(renderData, req.query.number_to_show);
+                //     res.render('./assets/pugs/average_values.pug', {
+                //         nav_class_input: "hidden",
+                //         nav_class_show: 'active-nav2',
+                //         sortData: renderDataLimit,
+                //         begin: req.query.period_begin,
+                //         end: req.query.period_end,
+                //         number_to_show: req.query.number_to_show
+                // //     });
+                // } else {
+                //     res.send('Još nije unešeno niti jedno mjerenje!!!\nMolimo unesite barem jedno mjerenje da bismo mogli pokazati prosječne vrijednosti.');
+                // }
             }
         });
     } else {

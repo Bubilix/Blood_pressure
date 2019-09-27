39f8a63f5f8a19f96b09f4c0947877ee4c8ed9c7
Gasparovic <sven.gasparovic@magna.com>
2019-09-27 14:11:57 +0200

    Resized xAxis date putput implemented

--- api/routers/graphics.js
+++ api/routers/graphics.js
@@ -17,21 +17,20 @@ router.get('/', mongoose_connection, (req, res, next) => {
                     return (Date.parse(input.time) <= Date.parse(req.query.period_begin) + (23.99 * 60 * 60000))
                     && (Date.parse(input.time) >= Date.parse(req.query.period_end));
                 });
-                // const inputs = user.inputs.filter(function(input) {
-                //     return (Date.parse(input.time) <= Date.now())
-                //     && (Date.parse(input.time) >= (Date.now() - (7 * 24 * 60 * 60000)));
-                // });
+                const timeString = [];
+                inputs.forEach(input => {
+                    timeString.push(input.time.toISOString().slice(0, 10));
+                });
                 if (inputs.length > 0) {
-                    const objToArr = [[], [], []];
+                    const objToArr = [[], []];
                     inputs.forEach(input => {
-                        objToArr[0].push(input.time);
-                        objToArr[1].push(input.upperValue);
-                        objToArr[2].push(input.lowerValue);
+                        objToArr[0].push(input.upperValue);
+                        objToArr[1].push(input.lowerValue);
                     });
                     res.render('./assets/pugs/graphics.pug', {
-                        upperValues: JSON.stringify(objToArr[1]),
-                        lowerValues: JSON.stringify(objToArr[2]),
-                        time: JSON.stringify(objToArr[0])
+                        upperValues: JSON.stringify(objToArr[0]),
+                        lowerValues: JSON.stringify(objToArr[1]),
+                        time: JSON.stringify(timeString)
                     });
                 } else {
                     res.render('./assets/pugs/no_output.pug');
